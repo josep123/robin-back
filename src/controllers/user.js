f8a3c57163c68674e964083451da9b122ee420b7
DESKTOP-96TI49E\user <joselrodriguez@festina.com>
2023-07-11 11:28:45 +0200

    basic controllers and model of user

---
+++ src/controllers/user.js
@@ -0,0 +1,52 @@
+const {Router} = require('express');
+const {User} = require('../..');
+const userRouter = Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+// Creo la ruta post para el registro
+
+
+userRouter.post('/register', async (req, res) => {
+
+    const saltRounds = 10;
+    const { name, dogeCoin, email, password} = req.body;
+    const hashPass = bcrypt.hashSync(password, saltRounds);
+    const user = new User({name: name, dogeCoin: dogeCoin, email: email, password: hashPass});
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {expiresIn: '10h' });
+    
+    userExist = User.findOne(req.body.email) 
+
+   let resultUser
+
+    if (!userExist) {
+    resultUser = "true";
+    } else { resultUser ="false";}
+
+
+
+
+ 
+
+  user.save((error, success) => {
+        if (error) {
+            res.status(500).json({error: error.message});
+        } else if (resultUser === "true"){
+            res.status(501).json({error: error.message});
+            } else {
+                res.status(201).json({ token: token, user: user  });
+                console.log('nuevo usuario creado!', hashPass);
+            }
+          
+           
+    })
+    
+})
+
+
+
+
+
+
+exports.userRouter = userRouter;
